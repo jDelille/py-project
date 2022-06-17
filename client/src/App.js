6edc79fdb79d560f98e9e6b9c8727d3aa1280da5
Justin Delille <justind7513@gmail.com>
2022-06-17 13:46:20 -0700

    setup client

--- client/src/App.js
+++ client/src/App.js
@@ -5,25 +5,18 @@ import './styles/App.scss';
 function App() {
 	const [data, setData] = useState([{}]);
 
-	useEffect(() => {
-		fetch('/results')
-			.then((res) => res.json())
-			.then((data) => {
-				setData(data);
-				console.log(data);
-			});
-	}, []);
+	// useEffect(() => {
+	// 	fetch('/results')
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			setData(data);
+	// 			console.log(data);
+	// 		});
+	// }, []);
 
 	return (
 		<div className='App'>
 			<Navbar />
-			{typeof data.results === 'undefined' ? (
-				<p>Loading...</p>
-			) : (
-				data.results.map((result, i) => {
-					return <p key={i}>{result.name}</p>;
-				})
-			)}
 		</div>
 	);
 }
